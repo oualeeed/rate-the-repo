7014bf0d60708827f781818dfed60619e84cf5ed
Oualid elf <oualid.elferaoui@gmail.com>
2023-10-26 12:58:22 +0100

    Configured the emulator and eslint.

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-web";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React native! 💓</Text>
+      <Text>Hello Asmae 💓</Text>
+      <Button>hello</Button>
       <StatusBar style="auto" />
     </View>
   );
